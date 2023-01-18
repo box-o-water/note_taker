9f594b57855fac7004c6ea294d54bba14142a5c9
box-o-water <boxooowater@gmail.com>
2023-01-18 12:36:22 -0600

    refactor(api): consolidate api pathing, don't need too complicated with only one end point

--- public/routes/api.js
+++ public/routes/api.js
@@ -1,10 +1,65 @@
-const express = require('express');
+const router = require('express').Router();
 
-// Import modular routers
-const notesRouter = require('./notes');
+const { v4: uuidv4 } = require('uuid');
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require('../helpers/fsUtils');
 
-const app = express();
+// GET Route for retrieving all the notes
+router.get('/notes', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
 
-app.use('/notes', notesRouter);
+// GET Route for a specific note
+router.get('/notes/:note_id', (req, res) => {
+  const noteId = req.params.note_id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((note) => note.note_id === noteId);
+      return result.length > 0
+        ? res.json(result)
+        : res.json('No note with that ID');
+    });
+});
 
-module.exports = app;
+// DELETE Route for a specific note
+router.delete('/notes/:note_id', (req, res) => {
+  const noteId = req.params.note_id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all notes except the one with the ID provided in the URL
+      const result = json.filter((note) => note.note_id !== noteId);
+
+      // Save that array to the filesystem
+      writeToFile('./db/db.json', result);
+
+      // Respond to the DELETE request
+      res.json(`Item ${noteId} has been deleted 🗑️`);
+    });
+});
+
+// POST Route for a new note
+router.post('/notes', (req, res) => {
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuidv4(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error('Error in adding note');
+  }
+});
+
+module.exports = router;
